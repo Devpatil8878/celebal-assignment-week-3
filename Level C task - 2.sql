a203a879fb75ba411ccd17b4a00ca9db22b12764
Dev patil <105635790+Devpatil8878@users.noreply.github.com>
2024-06-07 14:27:26 +0530

    Update Level C task - 2.sql

--- Level C task - 2.sql
+++ Level C task - 2.sql
@@ -1,3 +1,43 @@
+
+CREATE TABLE Friends (
+    ID INT PRIMARY KEY,
+    Friend_ID INT
+);
+
+
+INSERT INTO Friends (ID, Friend_ID) VALUES
+(1, 2),
+(2, 3),
+(3, 4),
+(4, 1);
+
+
+CREATE TABLE Students (
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50)
+);
+
+
+INSERT INTO Students (ID, Name) VALUES
+(1, 'Ashley'),
+(2, 'Samantha'),
+(3, 'Julia'),
+(4, 'Scarlet');
+
+
+CREATE TABLE Packages (
+    ID INT PRIMARY KEY,
+    Salary DECIMAL(10, 2)
+);
+
+
+INSERT INTO Packages (ID, Salary) VALUES
+(1, 15.20),
+(2, 10.06),
+(3, 11.55),
+(4, 12.12);
+
+
 SELECT
     S1.Name
 FROM
